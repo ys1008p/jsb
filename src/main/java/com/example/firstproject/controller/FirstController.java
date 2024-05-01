d025821c87cced5119b85a325d8eabdbe7e51a24
yspark <ys1008p@gmail.com>
2024-05-01 15:45:35 +0900

    controller 추가

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,12 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping()
+    public String niceToMeetYou() {
+        return "greetings";
+    }
+}
