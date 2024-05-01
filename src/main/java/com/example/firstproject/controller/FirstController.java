b9373c499f4aafd1e0d9eb8e201c9bf4378bec22
yspark <ys1008p@gmail.com>
2024-05-01 22:28:45 +0900

    mustache 페이지 구성

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -3,13 +3,21 @@ package com.example.firstproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirstController {
     @GetMapping("hi")
     public String niceToMeetYou(Model model) {
-        model.addAttribute("username", "홍길동");
+        model.addAttribute("username", "김길동");
 
         return "greetings";
+    }
+
+    @GetMapping("bye")
+    public String goodBye(Model model) {
+        model.addAttribute("nickname", "김길동");
+
+        return "goodbye";
     }
 }
