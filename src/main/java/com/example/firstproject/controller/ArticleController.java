3f09901797ebad35fd61c518bc92a2bdab4f46b1
yspark <ys1008p@gmail.com>
2024-05-06 20:17:33 +0900

    println >>> log.info 로 리팩토링

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired  // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
@@ -21,13 +22,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString()); // form이 제대로 넘어왔는지 확인
+        log.info(form.toString()); // printLn >> 로깅 코드로 변경
+//        System.out.println(form.toString()); // form이 제대로 넘어왔는지 확인
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString()); //--- ➊ DTO가 엔티티로 잘 변환되는지 확인 출력
+        log.info(article.toString());
+//        System.out.println(article.toString()); //--- ➊ DTO가 엔티티로 잘 변환되는지 확인 출력
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString()); //----- ➋ article이 DB에 잘 저장되는지 확인 출력
+        log.info(saved.toString());
+//        System.out.println(saved.toString()); //----- ➋ article이 DB에 잘 저장되는지 확인 출력
         return "";
     }
 }
