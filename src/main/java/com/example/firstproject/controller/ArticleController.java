25a172d8a8c0f7db297c87fd7799bbd4a75e9cb3
yspark <ys1008p@gmail.com>
2024-05-06 17:45:35 +0900

    H2 DB 사용실습

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired  // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
+    private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -15,7 +21,13 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        System.out.println(form.toString()); // form이 제대로 넘어왔는지 확인
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString()); //--- ➊ DTO가 엔티티로 잘 변환되는지 확인 출력
+        // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString()); //----- ➋ article이 DB에 잘 저장되는지 확인 출력
         return "";
     }
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,6 +1,8 @@
 package com.example.firstproject.dto;
 
 
+import com.example.firstproject.entity.Article;
+
 public class ArticleForm {
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
@@ -16,5 +18,9 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
     }
 }
